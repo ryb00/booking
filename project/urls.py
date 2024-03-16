a794be7f11ece203b14ab07d52b68b0b934eb714
Olena <ryb00@i.ua>
2024-03-16 09:28:08 +0000

    Add authentication

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("", include("book.urls"), name="book-urls"),
     path('admin/', admin.site.urls),
 ]
